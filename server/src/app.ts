8cbec8346aba4d16136643f715a4b729d4bdb4f2
David Oduneye <davidoduneye1@gmail.com>
2024-12-14 19:08:24 -0500

    init

---
+++ server/src/app.ts
@@ -0,0 +1,15 @@
+// import express from 'express';
+// import dotenv from 'dotenv';
+
+// const app = express();
+
+// if (process.env.ENV === 'production') {
+//   // app.set('trust proxy', 1)
+//   // sess.cookie.secure = true;
+// } else {
+//   dotenv.config();
+// }
+
+// app.use(express.json());
+
+// app.listen(process.env.PORT || 4000);
