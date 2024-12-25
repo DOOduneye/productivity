f9a1596b8bb8a7c21e649e7d8f50d9e97ff56b25
David Oduneye <davidoduneye1@gmail.com>
2024-12-25 13:39:26 -0500

    feat: add TypeScript and production Docker setup changes

--- docker-build.sh
+++ docker-build.sh
@@ -2,7 +2,7 @@
 docker buildx create --use
 docker buildx build \
   --no-cache \
-  --platform linux/amd64,linux/arm64 \
+  --platform linux/amd64 \
   --push \
   --pull \
   -t davidoduneye/productivity:latest .
