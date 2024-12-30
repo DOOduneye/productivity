df30e1c9f83eef68bcc4897d5d3f0c5d5b479657
David Oduneye <davidoduneye1@gmail.com>
2024-12-30 13:20:02 -0500

    feat: enhance Docker and Prettier + disable deployment ci

--- docker-build.sh
+++ docker-build.sh
@@ -1,8 +1,8 @@
 # Create a new builder
 docker buildx create --use
 docker buildx build \
-  --no-cache \
-  --platform linux/amd64 \
-  --push \
-  --pull \
-  -t davidoduneye/productivity:latest .
+	--no-cache \
+	--platform linux/amd64 \
+	--push \
+	--pull \
+	-t davidoduneye/productivity:latest .
